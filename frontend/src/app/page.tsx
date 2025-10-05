4643784b30afafe6ec993fb58af64b0b4ec393cd
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-10-05 13:26:02 -0400

    feat: add base styling and components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center p-24">
+      <Button>Hello World</Button>
+      <Button variant={"outline"}>Hello World</Button>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
