450130226bd485a4e88af3088edfa3530d92f1ec
Thomas Huitema <thuitema35@gmail.com>
2025-10-05 16:38:32 -0400

    feat: create Relationship entity

---
+++ backend/src/main/java/com/exiger/supplierportal/model/Relationship.java
@@ -0,0 +1,29 @@
+package com.exiger.supplierportal.model;
+
+import com.exiger.supplierportal.enums.SupplierStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * Represents a relationship between supplier and client.
+ * Primary key is composite and consists of the supplierID and clientID.
+ * The supplierStatus indicates the current state of the supplier in the relationship.
+ */
+@Entity
+@Data
+@Table(name = "relationship")
+public class Relationship {
+    @Embedded RelationshipID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "supplier_status", nullable = false)
+    private SupplierStatus status;
+}
+
+@Embeddable
+@Data
+class RelationshipID implements Serializable {
+    private Long supplierID;
+    private Long clientID;
+}
