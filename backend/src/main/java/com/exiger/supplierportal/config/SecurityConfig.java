da8b13d588887cb132187b970303e1ffabd2e061
Thomas Huitema <thuitema35@gmail.com>
2025-10-07 15:53:34 -0400

    docs: add documentation for AuthDemo and SecurityConfig

--- backend/src/main/java/com/exiger/supplierportal/config/SecurityConfig.java
+++ backend/src/main/java/com/exiger/supplierportal/config/SecurityConfig.java
@@ -5,18 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+/**
+ * This class configures endpoint access permissions.
+ * By default, any anonymous request to any endpoint returns a 401 Unauthorized error.
+ * In this example, we require users to be authenticated when accessing /api/whoami using .authenticated().
+ * If the user tries to access it without being authenticated, the server returns 401 Unauthorized error
+ * We allow anyone to access all other endpoints using .permitAll()
+ */
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
-                        .requestMatchers("/api/whoami").authenticated()
-                )
-                .oauth2ResourceServer((srv) -> srv.jwt(Customizer.withDefaults()))
-                .build();
+        return http.authorizeHttpRequests(
+            (req) -> req.requestMatchers("/api/whoami").authenticated()
+                    .requestMatchers("/**").permitAll() // any
+            )
+            .oauth2ResourceServer((srv) -> srv.jwt(Customizer.withDefaults()))
+            .build();
     }
 }
--- backend/src/main/java/com/exiger/supplierportal/controller/AuthDemo.java
+++ backend/src/main/java/com/exiger/supplierportal/controller/AuthDemo.java
@@ -4,6 +4,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This is an example controller to show authentication requirements of endpoints
+ * /api/whoami requires users to be authenticated, while /api/hello allows for anonymous access
+ * See config/SecurityConfig.java
+ */
 @RestController
 public class AuthDemo {
     @GetMapping("/api/hello")
