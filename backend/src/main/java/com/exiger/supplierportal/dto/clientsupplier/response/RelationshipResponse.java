88b6bce9b038e4c75eb659ed51b0485b476ec714
Urjit Chakraborty <135136842+urjit-chakraborty@users.noreply.github.com>
2025-10-08 21:37:55 -0400

    feat: add starting point for relationship response dto

---
+++ backend/src/main/java/com/exiger/supplierportal/dto/clientsupplier/response/RelationshipResponse.java
@@ -0,0 +1,7 @@
+package com.exiger.supplierportal.dto.clientsupplier.response;
+
+/**
+ * DTO for sending client and supplier relationship data to the frontend?
+ */
+public class RelationshipResponse {
+}
