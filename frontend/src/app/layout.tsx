7dac824a557d065be93ce6c873b338a819a19bca
t4rush <64758360+t4rush@users.noreply.github.com>
2025-10-08 22:39:01 -0400

    Docker Image for frontend and compose
    Added Docker Images for frontend, and created .yml file for docker compose which also uses a postgresql database. Ran npx prettier --write , due to formatting error.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css"
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,9 +12,9 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const inter = Inter({ 
+const inter = Inter({
   variable: "--font-inter",
-  subsets: ["latin"] 
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -31,11 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
