6a5f122952f82e5673c9e8190908fed79126d4e5
James C Miller <james.miller.1363@gmail.com>
2025-10-15 21:03:08 -0400

    feat - add tables for supplier and client

--- backend/src/main/java/com/exiger/supplierportal/model/Client.java
+++ backend/src/main/java/com/exiger/supplierportal/model/Client.java
@@ -1,1 +1,23 @@
 package com.exiger.supplierportal.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "client")
+public class Client {
+
+  @Id 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "client_id")
+  private Long clientID;
+
+  @Column(name = "client_name", nullable = false)
+  private String clientName;
+
+  @OneToMany(mappedBy = "id.clientID", cascade = CascadeType.ALL)
+  private List<Relationship> relationships;
+
+}
--- backend/src/main/java/com/exiger/supplierportal/model/Supplier.java
+++ backend/src/main/java/com/exiger/supplierportal/model/Supplier.java
@@ -1,1 +1,31 @@
 package com.exiger.supplierportal.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+
+/**
+ * Represents a supplier in the system.
+ * 
+ * Each supplier has a unique ID and a name, and can be linked to multiple clients
+ * through a Relationship. The relationships list stores all connections
+ * between this supplier and various clients, along with their statuses.
+ */
+@Entity
+@Data
+@Table(name = "supplier")
+public class Supplier {
+
+  @Id 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "supplier_id")
+  private Long supplierID;
+
+  @Column(name = "supplier_name", nullable = false)
+  private String supplierName;
+
+  @OneToMany(mappedBy = "id.supplierID", cascade = CascadeType.ALL)
+  private List<Relationship> relationships;
+
+}
