490e36631468bc1aaeca4784370db2fed94c1813
James C Miller <james.miller.1363@gmail.com>
2025-10-15 21:28:33 -0400

    chore - add comment for client table

--- backend/src/main/java/com/exiger/supplierportal/model/Client.java
+++ backend/src/main/java/com/exiger/supplierportal/model/Client.java
@@ -4,6 +4,13 @@ import jakarta.persistence.*;
 import lombok.Data;
 import java.util.List;
 
+/**
+ * Represents a client in the system.
+ * 
+ * Each client has a unique ID and a name, and can be linked to multiple suppliers
+ * through a Relationship. The relationships list stores all connections
+ * between this supplier and various clients, along with their statuses.
+ */
 @Entity
 @Data
 @Table(name = "client")
