17b1798cc3e23bf018215d3ff98471dabc94c931
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-10-20 13:47:46 -0400

    fix: change cookie type to correctly pass it to backend

--- frontend/src/lib/fetch.ts
+++ frontend/src/lib/fetch.ts
@@ -1,50 +1,50 @@
 type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 type FetchWithAuthProps<Body = unknown> = {
-    path: string;
-    method: HttpMethod;
-    headers?: Record<string, string>;
-    body?: Body;
+  path: string;
+  method: HttpMethod;
+  headers?: Record<string, string>;
+  body?: Body;
 };
 
 export type FetchResult<T> = { ok: true; data: T } | { ok: false; error: string };
 
 export async function fetchWithAuth<TResponse = unknown, TBody = unknown>(
-    { path, method, headers = {}, body }: FetchWithAuthProps<TBody>,
-    responseFn?: (response: Response) => Promise<TResponse>
+  { path, method, headers = {}, body }: FetchWithAuthProps<TBody>,
+  responseFn?: (response: Response) => Promise<TResponse>
 ): Promise<FetchResult<TResponse>> {
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
 
-    try {
-        if (typeof window === 'undefined') {
-            const { cookies } = await import('next/headers');
-            const cookieStore = await cookies()
-            const jsessionID = cookieStore.get("JSESSIONID")?.value
-            if (jsessionID) {
-                headers['Authorization'] = `Bearer ${jsessionID}`
-            }
-        }
-
-        const response = await fetch(url, {
-            method,
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-                ...headers,
-            },
-            body: body && method !== "GET" ? JSON.stringify(body) : undefined,
-        });
-
-        if (response.ok) {
-            const data = responseFn ? await responseFn(response) : await response.json();
-            return { ok: true, data };
-        } else {
-            return { ok: false, error: await response.text() };
-        }
-    } catch (err) {
-        return {
-            ok: false,
-            error: "Unknown network error",
-        };
+  try {
+    if (typeof window === "undefined") {
+      const { cookies } = await import("next/headers");
+      const cookieStore = await cookies();
+      const jsessionID = cookieStore.get("JSESSIONID")?.value;
+      if (jsessionID) {
+        headers["Cookie"] = `JSESSIONID=${jsessionID}`;
+      }
     }
+
+    const response = await fetch(url, {
+      method,
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+      body: body && method !== "GET" ? JSON.stringify(body) : undefined,
+    });
+
+    if (response.ok) {
+      const data = responseFn ? await responseFn(response) : await response.json();
+      return { ok: true, data };
+    } else {
+      return { ok: false, error: await response.text() };
+    }
+  } catch (err) {
+    return {
+      ok: false,
+      error: "Unknown network error",
+    };
+  }
 }
