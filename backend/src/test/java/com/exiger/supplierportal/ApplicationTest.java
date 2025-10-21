c16de14da8ab69b8aaef1efe595aa61c7a6789bb
Thomas Huitema <thuitema35@gmail.com>
2025-10-21 16:31:49 -0400

    fix: add testing configurations to ApplicationTest to not fail

--- backend/src/test/java/com/exiger/supplierportal/ApplicationTest.java
+++ backend/src/test/java/com/exiger/supplierportal/ApplicationTest.java
@@ -2,8 +2,10 @@ package com.exiger.supplierportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = { "api.token=test-token-123" })
+@ActiveProfiles("test")
 class ApplicationTest {
 
     @Test
