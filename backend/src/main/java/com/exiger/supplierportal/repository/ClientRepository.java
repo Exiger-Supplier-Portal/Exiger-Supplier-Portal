934815c3baf94e0d2c66e722893b37b3c5ad6cf8
Rachel Li <146363155+RachelLi15@users.noreply.github.com>
2025-10-22 17:51:20 -0400

    chore: add comment to ClientRepository method

--- backend/src/main/java/com/exiger/supplierportal/repository/ClientRepository.java
+++ backend/src/main/java/com/exiger/supplierportal/repository/ClientRepository.java
@@ -18,6 +18,12 @@ public interface ClientRepository extends JpaRepository<Client, String>{
     @Query("SELECT c.clientID from Client c")
     List<String> findAllClientIDs();
 
-    // Return true if a client with clientID exists, false otherwise
+    /**
+     * Check if a client exists given clientID.
+     * Uses Spring Data JPA's automatic query generation.
+     * 
+     * @param clientID The id of the client to check.
+     * @return true if a client with given clientID already exists, false otherwise.
+     */
     boolean existsByClientID(String clientID);
 }
