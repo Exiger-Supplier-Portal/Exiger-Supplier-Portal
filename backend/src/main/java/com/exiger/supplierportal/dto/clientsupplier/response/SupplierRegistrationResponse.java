5a0de1a48c514aff349569c93f7872169350b3e2
Vincent Liu <vincentliu0136@gmail.com>
2025-10-21 20:10:38 -0400

    [feat] add annotations for DTO/clientsupplier request and responses

--- backend/src/main/java/com/exiger/supplierportal/dto/clientsupplier/response/SupplierRegistrationResponse.java
+++ backend/src/main/java/com/exiger/supplierportal/dto/clientsupplier/response/SupplierRegistrationResponse.java
@@ -3,19 +3,26 @@ package com.exiger.supplierportal.dto.clientsupplier.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  * DTO for sending supplier registration data
  */
+@Schema(description = "Response object containing supplier registration data")
 public class SupplierRegistrationResponse {
+    @Schema(description = "Unique identifier for the supplier registration", example = "[id]")
     private Long id;
 
+    @Schema(description = "Unique identifier for the client", example = "[client-id]")
     private String clientID;
 
+    @Schema(description = "Email address of the supplier", example = "test@supplier.com")
     private String supplierEmail;
 
+    @Schema(description = "Unique token for supplier registration", example = "550e8400-e29b-41d4-a716-446655440000")
     private UUID token;
 
+    @Schema(description = "Expiration date of the supplier registration", example = "2025-12-31 11:59:59")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime expiration;
 }
--- backend/src/main/java/com/exiger/supplierportal/dto/clientsupplier/response/SupplierResponse.java
+++ backend/src/main/java/com/exiger/supplierportal/dto/clientsupplier/response/SupplierResponse.java
@@ -3,19 +3,24 @@ package com.exiger.supplierportal.dto.clientsupplier.response;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  * DTO for sending supplier data
  * Sends the ids for the client and supplier and the status of their relationship.
  */
+@Schema(description = "Response for sending supplier data")
 @Data
 public class SupplierResponse {
+    @Schema(description = "Unique identifier for the supplier", example = "[supplier-id]")
     @NotBlank
     private String supplierID;
 
+    @Schema(description = "Name of the supplier", example = "Test Supplier")
     @NotBlank
     private String supplierName;
 
+    @Schema(description = "Email of the supplier", example = "test@supplier.com")
     @NotBlank
     @Email(message = "Email should be valid")
     private String supplierEmail;
